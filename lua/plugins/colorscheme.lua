c6bf4721c1ffa3d165078d4c1da27190d8e3a401
Himanshu Tiwari <ht5620085@gmail.com>
2026-07-14 17:19:29 +0530

    changing theme to tokyonight night and adding some changes to the dashboard of the nvim logo

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup()
-      vim.cmd.colorscheme("catppuccin")
-    end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night", -- Set the style to night
+    transparent = false, -- Set to true if you want a transparent background
   },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
+
